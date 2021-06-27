1cadc691e3f1a87f657f67b6afb3da0592ca20f9
Sam Weddington <abakasam@outlook.com>
2021-06-27 21:36:57 +0300

    testing ionic

--- server/sql/firstinsert.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO ORDERS (PHONE)
-VALUES
-    (3469322511);
-
-INSERT INTO ORDERS (PHONE, ITEM, STREET, STAMP)
-VALUES
-    (3469322511, 'Strawberry Banana', 'Home', '04/17/2021');
-
--- Waiting to add menus and addresses for fake data
